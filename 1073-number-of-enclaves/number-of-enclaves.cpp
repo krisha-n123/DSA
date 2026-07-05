7d2c0f8a9eb6b259ce168876e5994d9592b5fb72
krisha-n123 <krisha6826@gmail.com>
2026-07-05 21:54:39 +0530

    Time: 8 ms (61.76%) | Memory: 30.5 MB (86.11%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,51 +1,59 @@
 class Solution {
 public:
-    int delr[4]={-1, 0 ,+1, 0};
-    int delc[4]={0,+1, 0, -1};
-    
-    void dfs(vector<vector<int>>& grid,vector<vector<int>>& vis, int r, int c){
-        vis[r][c]=1;
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int i=0;i<4;i++){
-            int nr=r+delr[i];
-            int nc=c+delc[i];
-            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !vis[nr][nc]){
-                dfs(grid, vis, nr, nc);
+    int delr[4] = {-1, 0, +1, 0};
+    int delc[4] = {0, +1, 0, -1};
+
+    void bfs(vector<vector<int>>& grid, int r, int c) {
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        grid[r][c] = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        while (!q.empty()) {
+            auto node=q.front();
+            q.pop();
+            int tr=node.first;
+            int tc=node.second;
+            for (int i = 0; i < 4; i++) {
+                int nr = tr + delr[i];
+                int nc = tc + delc[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                    grid[nr][nc] == 1) {
+                    q.push({nr, nc});
+                    grid[nr][nc] = 0;
+                }
             }
         }
-
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int count=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> vis(n, vector<int> (m , 0));
-        for(int i=0;i<m;i++){
-            //first row
-            if(grid[0][i]==1 && !vis[0][i]){
-                dfs(grid,vis, 0, i);
+        int count = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            // first row
+            if (grid[0][i] == 1) {
+                bfs(grid, 0, i);
             }
-            //last row
-            if(grid[n-1][i]==1 && !vis[n-1][i]){
-                dfs(grid,vis, n-1, i);
+            // last row
+            if (grid[n - 1][i] == 1) {
+                bfs(grid, n - 1, i);
             }
         }
-        for(int j=0;j<n;j++){
-            //first col 
-            if(grid[j][0]==1 && !vis[j][0]){
-                dfs(grid, vis, j, 0);
+        for (int j = 0; j < n; j++) {
+            // first col
+            if (grid[j][0] == 1) {
+                bfs(grid, j, 0);
             }
-            
+
             // last col
-            if(grid[j][m-1] && !vis[j][m-1]){
-                dfs(grid, vis, j , m-1);
+            if (grid[j][m - 1]) {
+                bfs(grid, j, m - 1);
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] && !vis[i][j]){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j]) {
                     count++;
                 }
             }
