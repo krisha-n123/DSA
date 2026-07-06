8f9d8251f7f1911b5e287bc5a29b1660b4d7244e
Krishan <krisha6826@gmail.com>
2026-07-06 15:47:52 +0530

    Time: 4 ms (90.05%) | Memory: 51.8 MB (91.63%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>& state, int i) {
-        //1 visiting chances of unsafe 
-        //2 safe 
-        if (state[i] == 1)
-            return false;
-        if(state[i] == 2)
-            return true;
+    bool dfs(vector<vector<int>>& adj, vector<bool>& vis,
+             vector<bool>& path, int curr, vector<int>& check) {
 
-        state[i]=1;
+        vis[curr] = true;
+        path[curr] = true;
+        check[curr] = 0;
 
-        for (int node : graph[i]) {
-            if(!dfs(graph, state, node)) return false;
-        }
-        state[i] = 2;
-            return true;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans;
-        vector<int> state(graph.size(), 0);
-        for (int i = 0; i < graph.size(); i++) {
-            if(dfs(graph, state, i)){
-                ans.push_back( i);
+        for (int v : adj[curr]) {
+
+            if (!vis[v]) {
+                if (dfs(adj, vis, path, v, check))
+                    return true;
+            }
+            else if (path[v]) {
+                return true;
+            }
+            else if (check[v] == 0) {
+                return true;
             }
         }
-        return ans;
+
+        check[curr] = 1;
+        path[curr] = false;
+        return false;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+
+        int V = graph.size();
+
+        vector<bool> vis(V, false);
+        vector<bool> path(V, false);
+        vector<int> check(V, 0);
+        vector<int> safe;
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i])
+                dfs(graph, vis, path, i, check);
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (check[i] == 1)
+                safe.push_back(i);
+        }
+
+        return safe;
     }
 };
