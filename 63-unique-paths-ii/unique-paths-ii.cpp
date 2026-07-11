ce2570181c84cbb904e4b47b45096a750105cd61
Krishan <krisha6826@gmail.com>
2026-07-12 00:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (31.23%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> dp;
+    int dfs(vector<vector<int>>& grid, int i , int j){
+        if((i<0 || i>=n || j<0 || j>=m || grid[i][j]==1)) return 0;
+
+        if(i==n-1 && j==m-1) return 1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        return dp[i][j]=dfs(grid, i+1,j)+dfs(grid, i,j+1);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+       n= grid.size();
+       m= grid[0].size();
+       dp.assign(n, vector<int>(m,-1));
+       return dfs(grid, 0,0);  
+    }
+};
